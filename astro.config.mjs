b023182ea6aa55372dd40d6bb313ffe45496d9a6
sirlilpanda <sirlilpanda@gmail.com>
2024-11-05 00:57:29 +1300

    maybe it wants the adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,8 @@ import node from '@astrojs/node';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
@@ -25,7 +27,5 @@ export default defineConfig({
 
   output: 'hybrid',
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: cloudflare()
 });
